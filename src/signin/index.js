cbaa9b60b7637f598c4b0efcaa497327b1272792
Richard <richard.rggdev@gmail.com>
2017-04-20 01:09:05 -0500

    First Commit

--- src/signin/index.js
+++ src/signin/index.js
@@ -2,6 +2,7 @@ import page from 'page';
 import empty from 'empty-element';
 import template from './template';
 
+
 page('/signin', function(ctx, next) {
     var main = document.getElementById('main-container');
     empty(main).appendChild(template);
